3b1ed604cd01b74e572455bb8182448571aa6a69
jonbaird <jbaird29@gmail.com>
2021-05-18 18:02:31 -0400

    convert customer-contacts to dynamic rendering

--- app.py
+++ app.py
@@ -1,26 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json
+import MySQLdb
 import os
+from dotenv import load_dotenv, find_dotenv
+
 
 # Configuration
-
 app = Flask(__name__)
+load_dotenv(find_dotenv())
+
+# Set the variables in our application with those environment variables
+host = os.environ.get("340DBHOST")
+user = os.environ.get("340DBUSER")
+passwd = os.environ.get("340DBPW")
+db = os.environ.get("340DB")
+db_connection = MySQLdb.connect(host,user,passwd,db)
+
 
 # Routes 
-
 @app.route('/',methods=['GET'])
 def root():
     return render_template("index.j2")
 
 @app.route('/customer-contacts',methods=['GET'])
-def customerContacts():
-    return render_template("customer-contacts.j2")
+def customer_contacts():
+    query = "SELECT * FROM customer_contacts"
+    cursor = db_connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute(query, args=None)
+    db_connection.commit()
+    results = cursor.fetchall()
+    return render_template("customer-contacts.j2", table_data=results)
 
 @app.route('/houses',methods=['GET'])
 def houses():
     return render_template("houses.j2")
 
 @app.route('/job-workers',methods=['GET'])
-def jobWorkers():
+def job_workers():
     return render_template("job-workers.j2")
 
 @app.route('/jobs',methods=['GET'])
@@ -32,7 +47,7 @@ def lawnmowers():
     return render_template("lawnmowers.j2")
 
 @app.route('/sales-managers',methods=['GET'])
-def salesManagers():
+def sales_managers():
     return render_template("sales-managers.j2")
 
 @app.route('/workers',methods=['GET'])
